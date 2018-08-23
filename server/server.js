7ef96a04cee4b6d8674fc8a57e869f712d64d6b3
kartikeya0169 <kartikeyemittal@gmail.com>
2018-08-23 22:30:36 +0530

    'emitnewmsg'

--- server/server.js
+++ server/server.js
@@ -1,11 +1,30 @@
 const path=require('path');
+const http=require('http');
 const express=require('express');
+const socketio=require('socket.io');
 
 const port=process.env.PORT||3000;
 const publicPath=path.join(__dirname,'../public');
 var app=express();
+var server=http.createServer(app);
+var io=socketio(server);
 
 app.use(express.static(publicPath));
-app.listen(port,()=>{
+io.on('connection',(socket)=>{
+    console.log('new user connected');
+
+
+    socket.on('createmsg',function(msg){
+      console.log(msg);
+      io.emit('newMessage',{
+        from:msg.from,
+        text:msg.text,
+        createdat:new Date().getTime()
+      })
+    });
+});
+
+
+server.listen(port,()=>{
   console.log('server is up at'+port);
 });
